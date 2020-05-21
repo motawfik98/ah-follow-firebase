277b2cd76001b211cbc7381f143a2335ae2d1bc1
Mohamed Tawfik <motawfik98@outlook.com>
2020-05-22 01:56:49 +0200

    Filtering tasks (doesn't search for text, and the following user can't search for a workingOnUser)

--- src/getTasks.js
+++ src/getTasks.js
@@ -1,6 +1,6 @@
 import * as firebaseConfig from '@/firebaseConfig'
 
-export async function getTasks(classification, currentUserUID, lastVisible) {
+export async function getTasks(classification, currentUserUID, lastVisible, searchingValues) {
     let tasks = []
     let document = []
     let fetchQuery = firebaseConfig.tasksCollection
@@ -12,6 +12,19 @@ export async function getTasks(classification, currentUserUID, lastVisible) {
     } else if (classification === 3) {
         fetchQuery = fetchQuery.where("workingOnUsersIDs", "array-contains", currentUserUID)
     }
+
+    if (searchingValues.workingOnUser !== null) {
+        fetchQuery = fetchQuery.where('workingOnUsersIDs', 'array-contains-any', [searchingValues.workingOnUser.uid])
+    }
+    if (searchingValues.startDate !== '') {
+        let date = new Date(searchingValues.startDate)
+        fetchQuery = fetchQuery.where('updatedAt', '>=', date)
+    }
+    if (searchingValues.endDate !== '') {
+        let date = new Date(searchingValues.endDate)
+        fetchQuery = fetchQuery.where('updatedAt', '<=', date)
+    }
+
     fetchQuery = fetchQuery.orderBy("updatedAt", "desc").limit(12)
 
     if (!(lastVisible === null || lastVisible === undefined)) {
